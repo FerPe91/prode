3a50488e0924337057f47ac5dbe884633f75f5ce
FerPe91 <fernando.pereyra764@gmail.com>
2023-01-09 17:04:15 -0300

    se agrego modal a saldo

--- PHP/home.php
+++ PHP/home.php
@@ -51,7 +51,8 @@
                 <div class="misDatos col-12 w-50 container h-100 bg-secondary">
                     <h3>MIS DATOS</h3>
                 </div>
-                <div class="saldo col-12 w-50 container h-100 bd-highlight bg-light">
+                    <!-- Button trigger modal -->
+                <div type="button" class="saldo col-12 w-50 container h-100 bd-highlight bg-light border-0 rounded-0 btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <h3>SALDO</h3>
                 </div>
             </div>
@@ -125,6 +126,36 @@
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content rounded-0">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">CARGAR SALDO</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="saldo" class="form-label">CANTIDAD</label>
+                                <input type="number" class="form-control" id="saldo" aria-describedby="emailHelp" placeholder="ej: 500">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">INGRESA TU CLAVE</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
